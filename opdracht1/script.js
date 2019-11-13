e9b01b54f3d829a967ee171e24d4a79c74973852
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-11-13 11:43:22 +0100

    prompt werkt en gebruikt en backgroundcolor gedaan

--- opdracht1/script.js
+++ opdracht1/script.js
@@ -2,7 +2,7 @@
 
 // jouw code hierondeer-->> 
 var getal1 = 23;
-var getal2 = 19;
+var getal2 = 16;
 
 // B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
 
@@ -11,6 +11,8 @@ document.getElementById("getal1").innerHTML = getal1;
 document.getElementById("getal2").innerHTML = getal2;
 // C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
 // Gebruik de prompt()-waarde om een variabelen te 'vullen'
+var getal2 = prompt ("wat is je naam")
+document.getElementById("getal1").innerHTML = getal2;
 
 // jouw code hieronder-->>
 //var ;
@@ -19,7 +21,8 @@ document.getElementById("getal2").innerHTML = getal2;
 // D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
 
 // jouw code hieronder-->>
-//if () {
-
-// }
+if (getal1 < getal2) {
+    document.getElementById("getallen").style.backgroundColor = "blue";
+    
+}
 
